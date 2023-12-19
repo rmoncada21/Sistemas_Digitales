a9ccab36189408fbe58d2e4c8d26e8788894e034
roma21fv <106123923+rmoncada21@users.noreply.github.com>
2023-12-19 13:05:49 -0600

    chaneg folder names

--- Morris_Mano/Circuitos_Combinacionales/Ejemplos_HDL/Compuertas_Tres_Estados/Descripcion.md
+++
@@ -1,5 +0,0 @@
-# Compuertas de 3 estados
-
-## Compuerta bufif1 
-- Se comporta como un búfer normal, si control=1
-- La salida pasa a un estado de alta impedancia z cuando control=0
---
+++ Morris_Mano/Circuitos_combinacionales/Ejemplos_HDL/Compuertas_tres_estados/tri_mux.v
@@ -0,0 +1,8 @@
+// Descripcion HDL usando el tipo de datos "tri" para la salida
+module tri_mux2x1 (A,B,select,OUT);
+    input A, B, select;
+    output OUT;
+    tri OUT;
+    bufif1 (OUT, A, select);
+    bufif0 (OUT, B, select);
+endmodule
