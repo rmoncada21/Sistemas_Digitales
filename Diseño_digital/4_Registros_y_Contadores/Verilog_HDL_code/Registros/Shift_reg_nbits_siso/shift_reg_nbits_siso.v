a45e02bf13c8f3674bdf30a4ba9becaac1601d2b
RomaF41 <106123923+rmoncada21@users.noreply.github.com>
2025-03-28 23:13:00 -0600

    Verilog HDL code: A simple right shift nbits resgiters using #(parameter N=4)

---
+++ Diseño_digital/4_Registros_y_Contadores/Verilog_HDL_code/Registros/Shift_reg_nbits_siso/shift_reg_nbits_siso.v
@@ -0,0 +1,9 @@
+module shift_reg_nbits_siso #(parameter N=4)(
+    input clk, si, 
+    output reg [N-1:0] shift
+);
+
+    always @(posedge clk) begin
+        shift <= {si, shift[N-1,1]};
+    end
+endmodule
